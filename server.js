9c4301fed382fd014c2f9945fdfad94f15f1f7c5
ok-barracuda <jiyoung.choi@yale.edu>
2015-01-28 02:52:54 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656!I am hungry!!');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('ok-barracuda');
 });
 
 app.listen(process.env.PORT || 4000);
